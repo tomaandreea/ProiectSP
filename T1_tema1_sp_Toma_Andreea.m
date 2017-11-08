865c3cc4e8432d265a1e0cefbb6448775f85a0be
tomaandreea <33005941+tomaandreea@users.noreply.github.com>
2017-11-08 19:44:00 +0200

    Add files via upload

---
+++ T1_tema1_sp_Toma_Andreea.m
@@ -0,0 +1,9 @@
+function[]=T1_tema1_sp_Toma_Andreea(t,b)
+duty = 1/4;
+%de asemenea inmultesc cu (1-n) si scad cu n deoarece nivelurile
+%maxim si minim nu sunt opuse (a+b = 0.5 (nivelul max) si a-b = -1 (nivelul
+%min))
+signal=(1-duty)*square(t*2*pi, duty*100) - duty;
+figure(b)
+plot(t,signal,'-r.'),title('Semnal dreptunghiular periodic'),xlabel('Timp [s]'),ylabel('A [V]'),grid
+end
