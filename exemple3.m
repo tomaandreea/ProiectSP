a71cc5c018bb9afb9e4fb78262f1a8f77f5fae62
tomaandreea <33005941+tomaandreea@users.noreply.github.com>
2017-11-08 20:14:12 +0200

    Delete exemple3.m

--- exemple3.m
+++
@@ -1,4 +0,0 @@
-T = 15;
-T1_tema3_sp_Toma_Andreea(0:0.02:T,1) % pentru 2ms = 0.002s
-T1_tema3_sp_Toma_Andreea(0:0.02:T,2) % pentru 20ms = 0.02s
-T1_tema3_sp_Toma_Andreea(0:0.2:T,3) % pentru 200ms = 0.2s
